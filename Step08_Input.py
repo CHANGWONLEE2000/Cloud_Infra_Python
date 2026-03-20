534d44944446b1aa2dd7e55e2f0c2fde2d8347ea
Leechangwon <fa-ct091238@naver.com>
2026-03-20 10:20:16 +0900

    Step08 추가함

---
+++ Step08_Input.py
@@ -0,0 +1,18 @@
+#Step08_Input.py
+
+
+# 콘솔창으로 부터 문자열 입력받기
+input_msg = input("메세지 입력: ")
+
+print(f"입력한 메세지: {input_msg} ")
+
+input_name : str = input("이름 입력: ")
+input_addr : str = input("주소 입력: ")
+
+print(f"이름: {input_name} 주소: {input_addr}")
+
+#문자열로 입력 받은후
+input_age: str = input("나이 입력: ")
+#숫자로 변환
+age : int = int(input_age)
+print(f"당신은 내년에 {age + 1} 살 입니다.")
